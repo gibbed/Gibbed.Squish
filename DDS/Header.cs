faf63c5655177997b326b4e621f3e91679192c33
Rick Gibbed <rick@gibbed.us>
2011-08-26 14:54:47 +0000

    Add Gibbed.Squish.

---
+++ DDS/Header.cs
@@ -0,0 +1,75 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System.IO;
+using Gibbed.IO;
+
+namespace Gibbed.Squish.DDS
+{
+    public class Header
+    {
+        public Header()
+        {
+            this.PixelFormat = new PixelFormat();
+        }
+
+        public uint GetSize()
+        {
+            return (18 * 4) + this.PixelFormat.GetSize() + (5 * 4);
+        }
+
+        public uint Size;
+        public HeaderFlags Flags;
+        public int Height;
+        public int Width;
+        public uint PitchOrLinearSize;
+        public uint Depth;
+        public uint MipMapCount;
+        public byte[] Reserved1 = new byte[11 * 4];
+        public PixelFormat PixelFormat;
+        public uint SurfaceFlags;
+        public uint CubemapFlags;
+        public byte[] Reserved2 = new byte[3 * 4];
+
+        public void Deserialize(Stream input, bool littleEndian)
+        {
+            this.Size = input.ReadValueU32(littleEndian);
+            this.Flags = input.ReadValueEnum<HeaderFlags>(littleEndian);
+            this.Height = input.ReadValueS32(littleEndian);
+            this.Width = input.ReadValueS32(littleEndian);
+            this.PitchOrLinearSize = input.ReadValueU32(littleEndian);
+            this.Depth = input.ReadValueU32(littleEndian);
+            this.MipMapCount = input.ReadValueU32(littleEndian);
+            if (input.Read(this.Reserved1, 0, this.Reserved1.Length) != this.Reserved1.Length)
+            {
+                throw new EndOfStreamException();
+            }
+            this.PixelFormat.Deserialize(input, littleEndian);
+            this.SurfaceFlags = input.ReadValueU32(littleEndian);
+            this.CubemapFlags = input.ReadValueU32(littleEndian);
+            if (input.Read(this.Reserved2, 0, this.Reserved2.Length) != this.Reserved2.Length)
+            {
+                throw new EndOfStreamException();
+            }
+        }
+    }
+}
---
+++ DDS/PixelFormat.cs
@@ -0,0 +1,202 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.IO;
+using Gibbed.IO;
+
+namespace Gibbed.Squish.DDS
+{
+    public class PixelFormat
+    {
+        public uint Size;
+        public PixelFormatFlags Flags;
+        public uint FourCC;
+        public uint RGBBitCount;
+        public uint RedBitMask;
+        public uint GreenBitMask;
+        public uint BlueBitMask;
+        public uint AlphaBitMask;
+
+        public uint GetSize()
+        {
+            return 8 * 4;
+        }
+
+        public void Initialise(FileFormat fileFormat)
+        {
+            this.Size = this.GetSize();
+            
+            switch (fileFormat)
+            {
+                case FileFormat.DXT1:
+                {
+                    this.Flags = PixelFormatFlags.FourCC;
+                    this.RGBBitCount = 0;
+                    this.RedBitMask = 0;
+                    this.GreenBitMask = 0;
+                    this.BlueBitMask = 0;
+                    this.AlphaBitMask = 0;
+                    this.FourCC = 0x31545844; // "DXT1"
+                    break;
+                }
+
+                case FileFormat.DXT3:
+                {
+                    this.Flags = PixelFormatFlags.FourCC;
+                    this.RGBBitCount = 0;
+                    this.RedBitMask = 0;
+                    this.GreenBitMask = 0;
+                    this.BlueBitMask = 0;
+                    this.AlphaBitMask = 0;
+                    this.FourCC = 0x33545844; // "DXT3"
+                    break;
+                }
+
+                case FileFormat.DXT5:
+                {
+                    this.Flags = PixelFormatFlags.FourCC;
+                    this.RGBBitCount = 0;
+                    this.RedBitMask = 0;
+                    this.GreenBitMask = 0;
+                    this.BlueBitMask = 0;
+                    this.AlphaBitMask = 0;
+                    this.FourCC = 0x35545844; // "DXT5"
+                    break;
+                }
+
+                case FileFormat.A8R8G8B8:
+                {
+                    Flags = PixelFormatFlags.RGBA;
+                    RGBBitCount = 32;
+                    FourCC = 0;
+                    RedBitMask = 0x00FF0000;
+                    GreenBitMask = 0x0000FF00;
+                    BlueBitMask = 0x000000FF;
+                    AlphaBitMask = 0xFF000000;
+                    break;
+                }
+
+                case FileFormat.X8R8G8B8:
+                {
+                    Flags = PixelFormatFlags.RGB;
+                    RGBBitCount = 32;
+                    FourCC = 0;
+                    RedBitMask = 0x00FF0000;
+                    GreenBitMask = 0x0000FF00;
+                    BlueBitMask = 0x000000FF;
+                    AlphaBitMask = 0x00000000;
+                    break;
+                }
+
+                case FileFormat.A8B8G8R8:
+                {
+                    Flags = PixelFormatFlags.RGBA;
+                    RGBBitCount = 32;
+                    FourCC = 0;
+                    RedBitMask = 0x000000FF;
+                    GreenBitMask = 0x0000FF00;
+                    BlueBitMask = 0x00FF0000;
+                    AlphaBitMask = 0xFF000000;
+                    break;
+                }
+
+                case FileFormat.X8B8G8R8:
+                {
+                    Flags = PixelFormatFlags.RGB;
+                    RGBBitCount = 32;
+                    FourCC = 0;
+                    RedBitMask = 0x000000FF;
+                    GreenBitMask = 0x0000FF00;
+                    BlueBitMask = 0x00FF0000;
+                    AlphaBitMask = 0x00000000;
+                    break;
+                }
+
+                case FileFormat.A1R5G5B5:
+                {
+                    Flags = PixelFormatFlags.RGBA;
+                    RGBBitCount = 16;
+                    FourCC = 0;
+                    RedBitMask = 0x00007C00;
+                    GreenBitMask = 0x000003E0;
+                    BlueBitMask = 0x0000001F;
+                    AlphaBitMask = 0x00008000;
+                    break;
+                }
+
+                case FileFormat.A4R4G4B4:
+                {
+                    Flags = PixelFormatFlags.RGBA;
+                    RGBBitCount = 16;
+                    FourCC = 0;
+                    RedBitMask = 0x00000F00;
+                    GreenBitMask = 0x000000F0;
+                    BlueBitMask = 0x0000000F;
+                    AlphaBitMask = 0x0000F000;
+                    break;
+                }
+
+                case FileFormat.R8G8B8:
+                {
+                    Flags = PixelFormatFlags.RGB;
+                    FourCC = 0;
+                    RGBBitCount = 24;
+                    RedBitMask = 0x00FF0000;
+                    GreenBitMask = 0x0000FF00;
+                    BlueBitMask = 0x000000FF;
+                    AlphaBitMask = 0x00000000;
+                    break;
+                }
+
+                case FileFormat.R5G6B5:
+                {
+                    Flags = PixelFormatFlags.RGB;
+                    FourCC = 0;
+                    RGBBitCount = 16;
+                    RedBitMask = 0x0000F800;
+                    GreenBitMask = 0x000007E0;
+                    BlueBitMask = 0x0000001F;
+                    AlphaBitMask = 0x00000000;
+                    break;
+                }
+
+                default:
+                {
+                    throw new NotSupportedException();
+                }
+            }
+        }
+
+        public void Deserialize(Stream input, bool littleEndian)
+        {
+            this.Size = input.ReadValueU32(littleEndian);
+            this.Flags = input.ReadValueEnum<PixelFormatFlags>(littleEndian);
+            this.FourCC = input.ReadValueU32(littleEndian);
+            this.RGBBitCount = input.ReadValueU32(littleEndian);
+            this.RedBitMask = input.ReadValueU32(littleEndian);
+            this.GreenBitMask = input.ReadValueU32(littleEndian);
+            this.BlueBitMask = input.ReadValueU32(littleEndian);
+            this.AlphaBitMask = input.ReadValueU32(littleEndian);
+        }
+    }
+}
