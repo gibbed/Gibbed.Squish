c25ae9923da5753f750000a231fd6997a0236f12
gibbed <rick@gibbed.us>
2017-04-03 23:03:10 -0500

    Updated libsquish binaries (and updated Native binding).

--- Native.cs
+++ Native.cs
@@ -3,17 +3,18 @@ using System.Runtime.InteropServices;
 
 namespace Gibbed.Squish
 {
-	public sealed class Native
-	{
+    public sealed class Native
+    {
         public enum Flags
         {
+            // ReSharper disable InconsistentNaming
             None = 0,
 
             /// <summary>
             /// Use DXT1 compression.
             /// </summary>
             DXT1 = 1 << 0,
-            
+
             /// <summary>
             /// Use DXT3 compression.
             /// </summary>
@@ -25,24 +26,24 @@ namespace Gibbed.Squish
             DXT5 = 1 << 2,
 
             /// <summary>
+            /// Use BC4 compression.
+            /// </summary>
+            BC4 = 1 << 3,
+
+            /// <summary>
+            /// Use BC5 compression.
+            /// </summary>
+            BC5 = 1 << 4,
+
+            /// <summary>
             /// Use a slow but high quality colour compressor (the default).
             /// </summary>
-            ColourClusterFit = 1 << 3,
-            
+            ColourClusterFit = 1 << 5,
+
             /// <summary>
             /// Use a fast but low quality colour compressor.
             /// </summary>
-            ColourRangeFit = 1 << 4,
-
-            /// <summary>
-            /// Use a perceptual metric for colour error (the default).
-            /// </summary>
-            ColourMetricPerceptual = 1 << 5,
-            
-            /// <summary>
-            /// Use a uniform metric for colour error.
-            /// </summary>
-            ColourMetricUniform = 1 << 6,
+            ColourRangeFit = 1 << 6,
 
             /// <summary>
             /// Weight the colour by alpha during cluster fit (disabled by default).
@@ -53,33 +54,61 @@ namespace Gibbed.Squish
             /// Use a very slow but very high quality colour compressor.
             /// </summary>
             ColourIterativeClusterFit = 1 << 8,
+
+            /// <summary>
+            /// Source is BGRA rather than RGBA.
+            /// </summary>
+            SourceBGRA = 1 << 9,
+            // ReSharper restore InconsistentNaming
         }
 
-		private	static bool	Is64Bit()
-		{
-			return Marshal.SizeOf(IntPtr.Zero) == 8; 
-		}
+        private static bool Is64Bit()
+        {
+            return Marshal.SizeOf(IntPtr.Zero) == 8;
+        }
 
-		private sealed class Native32
-		{
-			[DllImport("squish32.dll", EntryPoint = "SquishCompressImage", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern void CompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba, int width, int height, [MarshalAs(UnmanagedType.LPArray)] byte[] blocks, int flags);
+        private sealed class Native32
+        {
+            public const string DllName = "squish32";
 
-            [DllImport("squish32.dll", EntryPoint = "SquishDecompressImage", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern void DecompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba, int width, int height, [MarshalAs(UnmanagedType.LPArray)] byte[] blocks, int flags);
-		}
-   
-		private sealed class Native64
-		{
-            [DllImport("squish64.dll", EntryPoint = "SquishCompressImage", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern void CompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba, int width, int height, [MarshalAs(UnmanagedType.LPArray)] byte[] blocks, int flags);
+            [DllImport(DllName, EntryPoint = "CompressImage", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void CompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba,
+                                                      int width,
+                                                      int height,
+                                                      [MarshalAs(UnmanagedType.LPArray)] byte[] blocks,
+                                                      int flags,
+                                                      [MarshalAs(UnmanagedType.LPArray)] float[] metric);
 
-            [DllImport("squish64.dll", EntryPoint = "SquishDecompressImage", CallingConvention = CallingConvention.Cdecl)]
-            internal static extern void DecompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba, int width, int height, [MarshalAs(UnmanagedType.LPArray)] byte[] blocks, int flags);
-		}
-	
-		private static void CallDecompressImage(byte[] rgba, int width, int height, byte[] blocks, int flags)
-		{
+            [DllImport(DllName, EntryPoint = "DecompressImage", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void DecompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba,
+                                                        int width,
+                                                        int height,
+                                                        [MarshalAs(UnmanagedType.LPArray)] byte[] blocks,
+                                                        int flags);
+        }
+
+        private sealed class Native64
+        {
+            public const string DllName = "squish64";
+
+            [DllImport(DllName, EntryPoint = "CompressImage", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void CompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba,
+                                                      int width,
+                                                      int height,
+                                                      [MarshalAs(UnmanagedType.LPArray)] byte[] blocks,
+                                                      int flags,
+                                                      [MarshalAs(UnmanagedType.LPArray)] float[] metric);
+
+            [DllImport(DllName, EntryPoint = "DecompressImage", CallingConvention = CallingConvention.Cdecl)]
+            internal static extern void DecompressImage([MarshalAs(UnmanagedType.LPArray)] byte[] rgba,
+                                                        int width,
+                                                        int height,
+                                                        [MarshalAs(UnmanagedType.LPArray)] byte[] blocks,
+                                                        int flags);
+        }
+
+        private static void CallDecompressImage(byte[] rgba, int width, int height, byte[] blocks, int flags)
+        {
             if (Is64Bit() == true)
             {
                 Native64.DecompressImage(rgba, width, height, blocks, flags);
@@ -87,14 +116,14 @@ namespace Gibbed.Squish
             else
             {
                 Native32.DecompressImage(rgba, width, height, blocks, flags);
-			}
-		}
+            }
+        }
 
-		public static byte[] DecompressImage(byte[] blocks, int width, int height, Flags flags)
-		{
-			var pixelOutput = new byte[width * height * 4];
-			CallDecompressImage(pixelOutput, width, height, blocks, (int)flags);
-			return pixelOutput;
-		}
-	}
+        public static byte[] DecompressImage(byte[] blocks, int width, int height, Flags flags)
+        {
+            var pixelOutput = new byte[width * height * 4];
+            CallDecompressImage(pixelOutput, width, height, blocks, (int)flags);
+            return pixelOutput;
+        }
+    }
 }
